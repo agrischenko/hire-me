d0cb44c4400d68ff9b1b06650e0936aae566cce0
Alexey Grischenko <agrischenko@avistar.com>
2020-08-06 21:38:17 +0300

    setup main configuration. error handling.

--- router.js
+++ router.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+
 router.get('/', function (req, res) {
     res.render('index');
+});
+
+router.get('/err', function (req, res) {
+    throw new Error('generate error');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const path = require('path');
-const router = require('./router');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const router = require('./router');
 app.use(router);
+
+app.use(function (req, res, next) {
+   next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+   // set locals, only providing error in development
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+   console.error(err);
+
+   // render the error page
+   res.status(err.status || 500);
+   switch (err.status) {
+      case 404:
+         res.render('error-404');
+         break;
+      default:
+         res.render('error');
+   }
+});
 
 app.listen(80, function () {
    console.log(`Started...`);
